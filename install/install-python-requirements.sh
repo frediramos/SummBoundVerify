b7cc2e23eaa5b4e6f9da7a25db0c8192d0559972
Frederico Ramos <frediramos@gmail.com>
2026-02-25 23:32:55 +0000

    Add 32-bit support to install

--- install/install-python-requirements.sh
+++ install/install-python-requirements.sh
@@ -8,22 +8,22 @@ set -euo pipefail
 
 source "colors.sh"
 
-DEFAULT_REQ_FILE="requirements.txt"
+DEFAULT_REQ_FILE="./requirements.txt"
 
 # Show help
 if [[ "${1:-}" == "-h" || "${1:-}" == "--help" ]]; then
     echo -e "${BLUE}Usage:${RESET} $0 <requirements_file>"
     echo
-    echo -e "${BLUE}Installs a requirements file into an existing virtualenv.${RESET}"
+    echo -e "${BLUE}Installs a requirements file in the current python environment.${RESET}"
     echo
     echo -e "${YELLOW}Defaults:${RESET}"
-    echo -e "  ${BLUE}requirements_file${RESET} = $DEFAULT_REQ_FILE"
+    echo -e "  ${BLUE}requirements${RESET} = $DEFAULT_REQ_FILE"
     exit 0
 fi
 
 req_file="${1:-$DEFAULT_REQ_FILE}"
 echo -e "${BLUE}Using requirements file:${RESET} $req_file"
-echo -e "${YELLOW}Warning${RESET}: installing in the current python environment"
+echo -e "${YELLOW}Warning${RESET}: installing in the current python environment (${VIRTUAL_ENV})"
 
 # Check requirements file exists
 if [[ ! -f "$req_file" ]]; then
@@ -36,4 +36,4 @@ echo -e "${BLUE}Installing Python requirements...${RESET}"
 python3 -m pip install --upgrade pip
 python3 -m pip install -r "$req_file"
 
-echo -e "${GREEN}✔ Requirements installed successfully in'${RESET}"
+echo -e "${GREEN}✔ Requirements installed successfully in ${VIRTUAL_ENV}'${RESET}"
