ea277d3ce6b9dc7018d454521528c448dea317a9
Frederico Ramos <frediramos@gmail.com>
2026-08-13 21:00:06 +0100

    Cleanup generators

--- src/summboundverify/validation_gen/test_gen/arg_gen/generators/default.py
+++ src/summboundverify/validation_gen/test_gen/arg_gen/generators/default.py
@@ -1,56 +1,95 @@
-from pycparser.c_ast import (
-    ID,
-    FuncCall,
-    ExprList,
-    BinaryOp,
-    Constant,
-    NodeVisitor
-)
+from pycparser.c_ast import BinaryOp, Constant, ExprList, FuncCall, ID, NodeVisitor
 
 from summboundverify.api import api_map
 from summboundverify.validation_gen.utils import (
     ARRAY_SIZE_MACRO,
+    FUEL_MACRO,
     POINTER_SIZE_MACRO,
-    FUEL_MACRO
 )
 
 
 class DefaultGen(NodeVisitor):
-    def __init__(self, name, vartype):
+    """Base generator providing common AST construction helpers.
 
-        # Size Macros
-        self.size_macros = {
-            'array': ID(ARRAY_SIZE_MACRO),
-            'ptr': ID(POINTER_SIZE_MACRO)
-        }
+    Generators derived from this class use these helpers to construct
+    symbolic values, struct values, sizes, and constants.
+    """
+
+    def __init__(self, name, vartype):
+        super().__init__()
 
         self.argname = name  # ID node
-        self.vartype = vartype  # String
+        self.vartype = vartype  # C type name
 
-        # Fuel Macro
+        self.size_macros = {
+            "array": ID(ARRAY_SIZE_MACRO),
+            "ptr": ID(POINTER_SIZE_MACRO),
+        }
+
         self.fuel = ID(FUEL_MACRO)
 
     def init_struct_rvalue(self, vartype):
-        vartype = self.vartype.replace(' ', '_')
-        rvalue = FuncCall(ID(f'create_{vartype}'), ExprList([self.fuel]))
-        return rvalue
+        """Build a call that initializes a symbolic struct value.
+
+        For example, for ``vartype="struct foo"``, produces:
+
+            create_struct_foo(fuel)
+        """
+        struct_type = vartype.replace(" ", "_")
+
+        return FuncCall(
+            name=ID(f"create_{struct_type}"),
+            args=ExprList([self.fuel]),
+        )
 
     def type_size(self, vartype):
-        return BinaryOp(op='*', left=FuncCall(ID('sizeof'), ExprList([ID(vartype)])), right=Constant('int', str(8)))
+        """Build an expression computing the size of a C type in bits.
+
+        For example, for ``vartype="int"``, produces an expression
+        equivalent to:
+
+            sizeof(int) * 8
+        """
+        return BinaryOp(
+            op="*",
+            left=FuncCall(
+                name=ID("sizeof"),
+                args=ExprList([ID(vartype)]),
+            ),
+            right=Constant("int", "8"),
+        )
 
     def symbolic_rvalue(self, name):
+        """Build a call that creates a named symbolic value.
+
+        For example, with ``name="x"`` and ``vartype="int"``, produces:
+
+            __sym_var_named("x", sizeof(int) * 8)
+        """
         call = ID(api_map().sym_var_named)
-        sizeof = ExprList([name, self.type_size(self.vartype)])
-        rvalue = FuncCall(call, sizeof)
-        return rvalue
+        args = ExprList([name, self.type_size(self.vartype)])
+
+        return FuncCall(name=call, args=args)
 
     def const_rvalue(self, const):
+        """Convert a Python constant to the corresponding C AST node.
+
+        Examples:
+
+            42     -> 42
+            "x"    -> 'x'
+            "foo"  -> "foo"
+
+        Returns ``None`` for unsupported values.
+        """
         if isinstance(const, int):
-            return Constant('int', str(const))
-        elif isinstance(const, str):
+            return Constant("int", str(const))
+
+        if isinstance(const, str):
             if len(const) == 1:
-                return Constant('char', const)
-            elif len(const) > 1:
-                return Constant('string', const)
+                return Constant("char", const)
+
+            if len(const) > 1:
+                return Constant("string", const)
 
         return None
--- src/summboundverify/validation_gen/test_gen/arg_gen/generators/struct_fields/primitive_field.py
+++ src/summboundverify/validation_gen/test_gen/arg_gen/generators/struct_fields/primitive_field.py
@@ -1,31 +1,60 @@
-from pycparser.c_ast import *
+from pycparser.c_ast import Assignment, Constant, ID, StructRef
 
 from ..default import DefaultGen
 
 
-# Primitive type struct field
 class PrimitiveFieldGen(DefaultGen):
+    """
+    Generate initialization code for a primitive struct field.
+
+    For example, given ``struct_name="foo"`` and ``field="x"``,
+    generates an assignment equivalent to:
+
+        struct_foo_instance->x = new_sym_var("x");
+    """
+
     def __init__(self, name, vartype, struct_name, field):
         super().__init__(name, vartype)
 
         self.struct_name = struct_name
         self.field = field
 
-    # e.g: struct->field = new_sym_var(32)
+    def _field_lvalue(self):
+        """Build the struct-field access.
+
+        For ``struct_name="foo"`` and ``field="x"``, produces the
+        equivalent of:
+
+            struct_foo_instance->x
+        """
+        return StructRef(
+            name=ID(f"struct_{self.struct_name}_instance"),
+            type="->",
+            field=ID(self.field),
+        )
+
+    def _rvalue(self):
+        """Build the symbolic value assigned to the field.
+
+        For ``field="x"``, produces the equivalent of:
+
+            new_sym_var("x")
+        """
+        return self.symbolic_rvalue(
+            Constant("string", f'"{self.field}"')
+        )
+
     def gen(self):
-        code = []
-        name = f'struct_{self.struct_name}_instance'
+        """Generate the assignment to the struct field.
 
-        # Make symbolic type
-        rvalue = self.symbolic_rvalue(Constant('string', f'\"{self.field}\"'))
+        Example:
 
-        # struct->field
-        lvalue = StructRef(name=ID(f'{name}'),
-                           type='->', field=ID(f'{self.field}'))
-
-        # Assemble declaration
-        decl = Decl(name, [], [], [], [], lvalue, rvalue, None)
-
-        code.append(decl)
-
-        return code
+            struct_foo_instance->x = new_sym_var("x");
+        """
+        return [
+            Assignment(
+                op="=",
+                lvalue=self._field_lvalue(),
+                rvalue=self._rvalue(),
+            )
+        ]
