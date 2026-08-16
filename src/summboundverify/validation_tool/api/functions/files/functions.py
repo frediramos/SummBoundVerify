cef1ff86fdaaa014d2e77f3cdb97ec7ae8fe6b9b
Frederico Ramos <frediramos@gmail.com>
2026-08-16 23:58:16 +0100

    Start implementing the symbolic file system

---
+++ src/summboundverify/validation_tool/api/functions/files/functions.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+from claripy.ast.bv import BV as BitVector
+from angr.state_plugins import SimStateGlobals
+
+from .fs2 import SymbolicFS
+
+from ...summary import CSummary
+from ...context import ValidationCTX
+
+
+class FileSummary(CSummary, ABC):
+    def __init__(self, ctx: ValidationCTX):
+        super().__init__(ctx)
+
+    @property
+    def fs(self) -> SymbolicFS:
+        plugin = self.state.globals
+        assert isinstance(plugin, SimStateGlobals)
+
+        fs = plugin["fs"]
+        assert isinstance(fs, SymbolicFS)
+
+        if fs.state is not self.state:
+            fs = fs.clone(self.state)
+            plugin["fs"] = fs
+
+        return fs
+
+    def load_int(self, v: BitVector):
+        if not self.is_symbolic(v):
+            return self.state.solver.eval(v)
+        return v
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class file_create(FileSummary):
+    def run(self, filename_addr):
+        filename = self.load_string(filename_addr)
+        fd = self.fs.create_file(filename)
+        print("Create fd: ", fd)
+        return fd
