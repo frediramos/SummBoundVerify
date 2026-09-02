35e6c72becf7a260141d5ede61036a0c6c1b37b0
Frederico Ramos <frediramos@gmail.com>
2026-09-02 20:08:11 +0100

    Implement `file_close`
    Plus, handle:
      - Delete open files
      - Proper shared files logic (close and copy)

---
+++ tests/files/testing/file_close1.c
@@ -0,0 +1,48 @@
+#define NULL ((void*)0)
+#define INT_SIZE (sizeof(int) * 8)
+#define LONG_SIZE (sizeof(long) * 8)
+#define CHAR_SIZE (sizeof(char) * 8)
+#define PTR_SIZE (sizeof(void*) * 8)
+
+#include <stdio.h>
+
+typedef void *symbolic;
+typedef int state_t;
+typedef unsigned int cnstr_t;
+typedef unsigned int result_t;
+typedef unsigned int list_t;
+
+symbolic __sym_var(size_t size) { return 0; }
+symbolic __sym_var_named(char *name, size_t size) { return 0; }
+symbolic __sym_var_array(char *name, size_t index, size_t size) { return 0; }
+
+int __is_certain(cnstr_t cnstr) { return 0; }
+int __is_sat(cnstr_t cnstr) { return 0; }
+int __is_symbolic(symbolic var) { return 0; }
+
+cnstr_t _EQ_(symbolic var1, symbolic var2) { return 0; }
+
+void __assert(cnstr_t cnstr) { }
+void __report_error(const char* filename, unsigned int line, const char* message){ return; }
+
+int __file_create(char* filename){ return 0; }
+int __file_exists(char* filename){ return 0; }
+int __file_delete(char* filename){ return 0; }
+int __file_open(char* filename){ return 0; }
+int __file_close(int fd){ return 0; }
+
+
+#define SIZE 3
+
+int main(){
+  
+  int ret1 = __file_create("abc");
+  __assert(ret1 == 1);
+  
+  int ret2 = __file_open("abc");
+  __assert(ret2 == 3);
+
+  int ret3 = __file_close(ret2);
+  __assert(ret3 == 0);
+
+}
